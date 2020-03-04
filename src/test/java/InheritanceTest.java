e17505ce8bd9b3cbb48b3d842e27dac2c80827e1
215031393@mycput.ac.za <Abr*84084>
2020-03-04 19:44:37 +0200

    'l'

---
+++ src/test/java/InheritanceTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InheritanceTest {
+
+    @Test
+    public void setFaculty() {
+    }
+
+    @Test
+    public void getFaculty() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
